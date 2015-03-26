67eebd1435c5cfea6af96ebc89946df03a23fb3b
Lee Brandt <leebrandt@gmail.com>
2015-03-26 15:41:00 -0500

    Karma set up. Karma init run Gulp set up. Gulp tasks for inject and karma setup. bower set up. installed local versions of angular, et. al. gulp running unit tests and injecting custom scripts.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,29 @@
 var gulp = require('gulp'),
-		inject = require('gulp-inject');
+		inject = require('gulp-inject'),
+		karma = require('gulp-karma');
 
 var paths = {
-	scripts: ['modules/weather/weather.module.js', 'modules/**/*.js']
+	scripts: ['modules/weather/weather.module.js', 'modules/**/*.js', '!modules/**/*.test.js'],
+  unitTests: ['modules/**/*.test.js']
 };
 
 gulp.task('inject', function(){
 	return gulp.src('index.html')
 		.pipe(inject(gulp.src(paths.scripts, {read:false})))
 		.pipe(gulp.dest('./'));
+});
+
+gulp.task('test:unit', function(){
+	return gulp.src('./foobar')
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }))
+    .on('error', function(err) {
+      // Make sure failed tests cause gulp to exit non-zero
+      console.log(err);
+      this.emit('end'); //instead of erroring the stream, end it
+    });
 });
 
 gulp.task('default', ['inject'], function(){
---
+++ modules/weather/weather.module.test.js
@@ -0,0 +1,10 @@
+describe('Weather App Module', function(){
+	'use strict';
+
+	beforeEach(module('weatherApp'));
+
+	it('should be able to pass a test', function(){
+		expect(true).toBe(true);
+	});
+
+});
