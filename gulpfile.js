83ea5ba5ba1de5470211618865905135ff23121d
Lee Brandt <leebrandt@gmail.com>
2015-07-19 20:38:02 -0500

    Added browser-sync to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp'),
 		inject = require('gulp-inject'),
-		karma = require('gulp-karma');
+		karma = require('gulp-karma')
+		browserSync = require('browser-sync'),
+		reload = browserSync.reload;
 
 var paths = {
 	scripts: ['modules/weather/weather.module.js', 'modules/**/*.js', '!modules/**/*.test.js'],
+	html: ['**/*.html'],
   unitTests: ['modules/**/*.test.js']
 };
 
@@ -11,6 +14,14 @@ gulp.task('inject', function(){
 	return gulp.src('index.html')
 		.pipe(inject(gulp.src(paths.scripts, {read:false})))
 		.pipe(gulp.dest('./'));
+});
+
+gulp.task('browser-sync', function(){
+	browserSync({
+		server: {
+			baseDir: './'
+		}
+	});
 });
 
 gulp.task('test:unit', function(){
@@ -29,4 +40,9 @@ gulp.task('test:unit', function(){
 gulp.task('default', ['inject', 'test:unit'], function(){
 	gulp.watch(paths.scripts, ['inject']);
   gulp.watch(paths.unitTests, ['test:unit']);
+});
+
+gulp.task('dev', ['inject', 'browser-sync'], function(){
+	gulp.watch(paths.html, [reload]);
+	gulp.watch(paths.scripts, ['inject', reload]);
 });
